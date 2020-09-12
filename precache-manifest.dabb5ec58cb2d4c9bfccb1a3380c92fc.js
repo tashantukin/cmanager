a2b673106a45ff5d9838f4670d5700ad63f13972
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2020-09-12 17:42:05 +0800

    Updates

---
+++ precache-manifest.dabb5ec58cb2d4c9bfccb1a3380c92fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bfc84e42c64eec593645764fa938c9",
+    "url": "/cmanager/index.html"
+  },
+  {
+    "revision": "dd7dc742ff4a848c5e1c",
+    "url": "/cmanager/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "93793bf5ab55aa13f6bb",
+    "url": "/cmanager/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dd7dc742ff4a848c5e1c",
+    "url": "/cmanager/static/js/2.eced2c1c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cmanager/static/js/2.eced2c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93793bf5ab55aa13f6bb",
+    "url": "/cmanager/static/js/main.bc1f64d2.chunk.js"
+  },
+  {
+    "revision": "a4c5f272f705ffd5258f",
+    "url": "/cmanager/static/js/runtime-main.1c35afc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cmanager/static/media/logo.5d5d9eef.svg"
+  }
+]);
